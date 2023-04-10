75d639e6552a1a337233f232d93d97266b4c3e26
subhogangu0892@gmail.com <subhogangu0892@gmail.com>
2023-04-10 14:23:24 +0000

    first model

--- models/first_Model/me_customer.sql
+++ models/first_Model/me_customer.sql
@@ -1,10 +1,6 @@
-{{config(
-    schema='STAGING',
-    database='DEV',
-    materialized='table'
-)}}
+{{ config(schema="STAGING", database="DEV", materialized="table") }}
 
-select 
+select
     c_custkey as cust_key,
     c_name as cust_nm,
     c_address as cust_address,
@@ -12,8 +8,10 @@ select
     n_name as nation,
     r_name as regionname,
     c_acctbal as cust_acct_bal
-    
-from {{var('CUSTOMER_tbl')}}
-left join {{source('me_customer','NATION')}} on CUSTOMER.C_NATIONKEY=NATION.N_NATIONKEY
-left join {{source('me_customer','REGION')}} on NATION.N_REGIONKEY=REGION.R_REGIONKEY
-where r_name in ('{{var('region_name')}}')
+
+from {{ var("CUSTOMER_tbl") }}
+left join
+    {{ source("me_customer", "NATION") }} on customer.c_nationkey = nation.n_nationkey
+left join
+    {{ source("me_customer", "REGION") }} on nation.n_regionkey = region.r_regionkey
+where r_name in ('{{var(' region_name ')}}')
